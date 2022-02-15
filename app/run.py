1210934cd516eb5b2d29b550c2177cfe4ea494b0
AgentBG13 <beegee131242@gmail.com>
2022-02-15 16:18:05 +0700

    Add a bar graph

--- app/run.py
+++ app/run.py
@@ -43,9 +43,30 @@ def index():
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
     
+    category_names = df.iloc[:, 4:].columns
+    category_counts = (df.iloc[:, 4:]!=0).sum()
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
+        {
+            'data': [
+                Bar(
+                    x=category_names,
+                    y=category_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Categories',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Category"
+                }
+            }
+        },
+
         {
             'data': [
                 Bar(
